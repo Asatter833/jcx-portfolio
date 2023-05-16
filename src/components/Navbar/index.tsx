f0358cf1bc12c09607311c92fe1d2d4db558ad75
Asatter833 <asatter833@gmail.com>
2023-05-16 22:54:28 +0600

    menu ccomplete

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -5,6 +5,7 @@ import HoverButton from "../HoverButton";
 import Image from "next/image";
 import Logo from "../../../public/logo.svg";
 import MenuOverlay from "./MenuOverlay";
+import { Icon } from "@iconify/react";
 
 const Index = () => {
   const [navbarOpen, setNavbarOpen] = React.useState(false);
@@ -12,16 +13,18 @@ const Index = () => {
     <>
       <header className="w-full mx-auto sm:px-12 py-4 sticky z-20 top-0 left-0 bg-slate-900">
         <div className="flex flex-row sm:flex-row justify-between items-center z-20 px-6">
-          <div className="flex gap-12 items-center z-20 ">
+          <div className="flex gap-12 items-center ">
             <div
-              className="flex flex-row gap-2 justify-between items-center z-20"
+              className="flex flex-row pl-4 justify-between items-center"
               onClick={() => setNavbarOpen(!navbarOpen)}
             >
               <button
-                className="flex top-0 right-0 relative w-10 h-10 z-20 text-white focus:outline-none"
+                className={
+                  "flex top-0 right-0 relative w-10 h-10 z-20 text-white focus:outline-none transition ease-in-out delay-150 border hover:border-line-color rounded-full lg:border-none duration-500"
+                }
                 onClick={() => setNavbarOpen(!navbarOpen)}
               >
-                <div className="absolute w-5 transform -translate-x-1/2 -translate-y-1/2 left-1/2 top-1/2">
+                <div className="absolute w-5 transform -translate-x-1/2 -translate-y-1/2 left-1/4 top-1/2">
                   <span
                     className={`absolute h-px w-5 bg-line-color transform transition duration-400 ease-in-out ${
                       navbarOpen ? "rotate-45 delay-200" : "-translate-y-1.5"
@@ -41,12 +44,12 @@ const Index = () => {
                   ></span>
                 </div>
               </button>
-              <p className="text-xs font-medium text-slate-300 cursor-pointer">
+              <p className="text-xs font-medium text-slate-300 cursor-pointer lg:z-20 hidden lg:block">
                 Menu
               </p>
             </div>
           </div>
-          <div>
+          <div className={"z-20 sm:z-0"}>
             <Image
               src={Logo}
               width={90}
@@ -54,6 +57,15 @@ const Index = () => {
               alt="Picture of the author"
               priority
             />
+          </div>
+          <div className="z-20 sm:hidden ">
+            <button className="w-10 h-10  transition ease-in-out delay-150 border hover:border-line-color rounded-full lg:border-none duration-500">
+              <Icon
+                className="text-xl translate-x-1/2 -translate-y-1/6 right-1/4 top-1/2"
+                color="#f56a5b"
+                icon="ic:baseline-phone"
+              />
+            </button>
           </div>
           <div className="hidden sm:block">
             <HoverButton />
