5287247aef97f9b4ebb1391b2e32e97025131653
Asatter833 <asatter833@gmail.com>
2023-05-16 22:54:21 +0600

    menu complete

--- src/components/Navbar/MenuOverlay.tsx
+++ src/components/Navbar/MenuOverlay.tsx
@@ -7,145 +7,172 @@ const MenuOverlay: React.FC<{
   setNavbarOpen: any;
 }> = ({ navbarOpen, setNavbarOpen }) => {
   return (
-    <nav
-      className={`fixed flex  top-0 left-0 w-full sm:w-[85vw] md:w-[65vw] px-12 sm:px-24 z-10 h-screen pt-24 bg-gray-900 transform delay-100 transition duration-500 ease-in-out ${
-        navbarOpen ? "opacity-100 translate-x-0" : "opacity-0 -translate-x-full"
-      }`}
-    >
-      <div className="flex flex-col justify-between">
-        <div>
-          <ul>
-            <li className="nav-li">
+    <>
+      <div
+        className={`h-full w-full bg-slate-800 bg-opacity-40 ${
+          navbarOpen ? "fixed" : "hidden"
+        } top-[72px] left-0 transition duration-200
+        `}
+        // onClick={() => setNavbarOpen((n) => !n)}
+      />
+      <button className=""></button>
+      <nav
+        className={`fixed flex top-0 left-0 w-full sm:w-[85vw] md:w-[65vw] px-12 sm:px-24 z-10 h-screen pt-24 bg-gray-900 transform delay-100 transition duration-500 ease-in-out ${
+          navbarOpen
+            ? "opacity-100 translate-x-0"
+            : "opacity-0 -translate-x-full"
+        }`}
+      >
+        <div className="flex flex-col justify-between h-full w-full">
+          <div className="pb-2">
+            <ul>
+              <li className="nav-li">
+                <Link
+                  href="/"
+                  className="nav-link"
+                  onClick={() => {
+                    setNavbarOpen(false);
+                  }}
+                >
+                  <h1 className="[&>span]:text-line-color">
+                    HO<span>ME</span>
+                  </h1>
+                </Link>
+              </li>
+              <li className="nav-li">
+                <Link
+                  href="/about"
+                  className="nav-link"
+                  onClick={() => {
+                    setNavbarOpen(false);
+                  }}
+                >
+                  <h1 className="[&>span]:text-line-color">
+                    AB<span>OUT</span>
+                  </h1>
+                </Link>
+              </li>
+              <li className="nav-li">
+                <Link
+                  href="/projects"
+                  className="nav-link"
+                  onClick={() => {
+                    setNavbarOpen(false);
+                  }}
+                >
+                  <h1 className="[&>span]:text-line-color">
+                    PR<span>OPERTIES</span>
+                  </h1>
+                </Link>
+              </li>
+              <li className="nav-li">
+                <Link
+                  href="/projects"
+                  className="nav-link"
+                  onClick={() => {
+                    setNavbarOpen(false);
+                  }}
+                >
+                  <h1 className="[&>span]:text-line-color">
+                    GA<span>LLERY</span>
+                  </h1>
+                </Link>
+              </li>
+              <li className="nav-li">
+                <Link
+                  href="/news"
+                  className="nav-link"
+                  onClick={() => {
+                    setNavbarOpen(false);
+                  }}
+                >
+                  <h1 className="[&>span]:text-line-color">
+                    NE<span>WS & BLOGS</span>
+                  </h1>
+                </Link>
+              </li>
+              <li className="nav-li">
+                <Link
+                  href="/career"
+                  className="nav-link"
+                  onClick={() => {
+                    setNavbarOpen(false);
+                  }}
+                >
+                  <h1 className="[&>span]:text-line-color">
+                    CA<span>REER</span>
+                  </h1>
+                </Link>
+              </li>
+              <li className="nav-li">
+                <Link
+                  href="/contact"
+                  className="nav-link"
+                  onClick={() => {
+                    setNavbarOpen(false);
+                  }}
+                >
+                  <h1 className="[&>span]:text-line-color">
+                    CO<span>NTACT</span>
+                  </h1>
+                </Link>
+              </li>
+            </ul>
+          </div>
+
+          <div className="flex flex-col justify-between pb-14 gap-10">
+            <div>
               <Link
                 href="/"
-                className="nav-link"
+                className="text-sm text-white focus:outline-none tracking-normal opacity-50 hover:opacity-100 hover:pl-8 transition-all duration-200 ease-in-out"
                 onClick={() => {
                   setNavbarOpen(false);
                 }}
               >
-                <h1>HOME</h1>
+                <h6>PROJECTS PROGRESS</h6>
               </Link>
-            </li>
-            <li className="nav-li">
               <Link
-                href="/about"
-                className="nav-link"
+                href="/"
+                className="text-sm text-white focus:outline-none tracking-normal opacity-50 hover:opacity-100 hover:pl-8 transition-all duration-200 ease-in-out"
                 onClick={() => {
                   setNavbarOpen(false);
                 }}
               >
-                <h1>ABOUT</h1>
+                <h6>TEAM</h6>
               </Link>
-            </li>
-            <li className="nav-li">
               <Link
-                href="/projects"
-                className="nav-link"
+                href="/"
+                className="text-sm text-white focus:outline-none tracking-normal opacity-50 hover:opacity-100 hover:pl-8 transition-all duration-200 ease-in-out"
                 onClick={() => {
                   setNavbarOpen(false);
                 }}
               >
-                <h1>PROPERTIES</h1>
+                <h6>NEWS AND OFFERS</h6>
               </Link>
-            </li>
-            <li className="nav-li">
               <Link
-                href="/projects"
-                className="nav-link"
+                href="/"
+                className="text-sm text-white focus:outline-none tracking-normal opacity-50 hover:opacity-100 hover:pl-8 transition-all duration-200 ease-in-out"
                 onClick={() => {
                   setNavbarOpen(false);
                 }}
               >
-                <h1>GALLERY</h1>
+                <h6>WHY US!</h6>
               </Link>
-            </li>
-            <li className="nav-li">
-              <Link
-                href="/news"
-                className="nav-link"
-                onClick={() => {
-                  setNavbarOpen(false);
-                }}
-              >
-                <h1>NEWS & BLOGS</h1>
-              </Link>
-            </li>
-            <li className="nav-li">
-              <Link
-                href="/career"
-                className="nav-link"
-                onClick={() => {
-                  setNavbarOpen(false);
-                }}
-              >
-                <h1>CAREER</h1>
-              </Link>
-            </li>
-            <li className="nav-li">
-              <Link
-                href="/contact"
-                className="nav-link"
-                onClick={() => {
-                  setNavbarOpen(false);
-                }}
-              >
-                <h1>CONTACT</h1>
-              </Link>
-            </li>
-          </ul>
-        </div>
-
-        <div className="flex flex-row justify-between pb-14">
-          <div>
-            <Link
-              href="/"
-              className="text-sm text-white focus:outline-none tracking-normal opacity-50 hover:opacity-100 hover:pl-8 transition-all duration-200 ease-in-out"
-              onClick={() => {
-                setNavbarOpen(false);
-              }}
-            >
-              <h6>PROJECTS PROGRESS</h6>
-            </Link>
-            <Link
-              href="/"
-              className="text-sm text-white focus:outline-none tracking-normal opacity-50 hover:opacity-100 hover:pl-8 transition-all duration-200 ease-in-out"
-              onClick={() => {
-                setNavbarOpen(false);
-              }}
-            >
-              <h6>TEAM</h6>
-            </Link>
-            <Link
-              href="/"
-              className="text-sm text-white focus:outline-none tracking-normal opacity-50 hover:opacity-100 hover:pl-8 transition-all duration-200 ease-in-out"
-              onClick={() => {
-                setNavbarOpen(false);
-              }}
-            >
-              <h6>NEWS AND OFFERS</h6>
-            </Link>
-            <Link
-              href="/"
-              className="text-sm text-white focus:outline-none tracking-normal opacity-50 hover:opacity-100 hover:pl-8 transition-all duration-200 ease-in-out"
-              onClick={() => {
-                setNavbarOpen(false);
-              }}
-            >
-              <h6>WHY US!</h6>
-            </Link>
-          </div>
-          <div className="text-white opacity-50">
-            <p>
-              JCX Business Tower Plot 1136/A,
-              <br />
-              Japan Street, Block # I, <br />
-              Bashundhara R/A, Dhaka - 1229,
-              <br /> Bangladesh
-            </p>
+            </div>
+            <div className="border-b border-line-color" />
+            <div className="text-white opacity-70">
+              <p>
+                JCX Business Tower Plot 1136/A,
+                <br />
+                Japan Street, Block # I, <br />
+                Bashundhara R/A, Dhaka - 1229,
+                <br /> Bangladesh
+              </p>
+            </div>
           </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 };
 
